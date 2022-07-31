c14a6d2dc1b669e7ec760fd39fbee0964e566b24
edgar.landero <edgar.landero@corpogas.com.mx>
2022-07-31 15:36:11 -0500

    carga de NETCORE Project

---
+++ Data/Models/Testing.cs
@@ -0,0 +1,28 @@
+﻿namespace WorldCitiesAPI.Data.Models
+{
+    public class Testing: Country
+    {
+      
+        public Testing()
+        {
+            Testing test = new Testing();
+        }
+
+        public bool OtraCosa()
+        {
+
+            //protected
+            //public
+            int result = 0;
+            string algoNuevo = this.ISO3;
+
+
+          result =  this.DoSomething();
+
+            return true;
+        }
+        
+
+
+    }
+}
